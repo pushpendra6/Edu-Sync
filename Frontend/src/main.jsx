16bd5283a720d715d5d351c43f2473e2cbbe05a2
pushpendra6 <shekhawatpss9783@gmail.com>
2025-07-18 19:44:31 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,6 +5,8 @@ import App from "./App.jsx";
 import { ToastContainer } from "react-toastify";
 import { Provider } from "react-redux";
 import store from "./Redux/Store.jsx";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 createRoot(document.getElementById("root")).render(
 	// <StrictMode>
